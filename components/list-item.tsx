66a92c432e1a6416039d70cbc6eeb53cc61ba5fa
davidebiscuso <biscu@live.it>
2024-12-18 20:51:58 +0100

    fixed type

--- components/list-item.tsx
+++ components/list-item.tsx
@@ -1,7 +1,12 @@
 import Image from 'next/image';
 import { Bell, MessageCircle, User, ChevronRight } from 'lucide-react';
 
-export function ListItem({ noIcon }) {
+interface ListItemProps {
+  noIcon?: boolean;
+}
+
+export function ListItem(props: ListItemProps) {
+  const { noIcon } = props;
   return (
     <div className="bg-neutral-100 border-b-[1px] w-full p-4 flex flex-row justify-between items-center dark:bg-neutral-800 dark:border-neutral-700">
       <span>List item</span>
